b329376f052a8880ddc4e6587078303709300a85
HelHitch <olena9002@gmail.com>
2022-07-17 14:07:43 +0400

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,49 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync");
-const sass = require("gulp-sass").require("sass");
+const sass = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
 
-// Static server
+// Создаем сервер
 gulp.task("server", function () {
   browserSync.init({
     server: {
-      baseDir: "src",
+      baseDir: "src", //указываем дерикторию с index.html
     },
   });
 });
 
+//отслеживание изменений стилей
 gulp.task("styles", function () {
-  return gulp
-    .src("src/sass/*.+(scss|sass)") //даем тип препроцессора на выбор
-    .pipe(sass())
-    .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
+  return (
+    gulp
+      .src("src/sass/*.+(sass|scss)") //возвращаем стили из папки ресурсов сасс, с одним из препроцессоров на выбор
+      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError)) //функция компрессирования
+      //добавляем суффикс со значением мин, чтобы изменения уходили в стайл.мин.цсс
+      .pipe(
+        rename({
+          suffix: ".min",
+        })
+      )
+      .pipe(
+        autoprefixer({
+          cascade: false,
+        })
+      )
+      .pipe(cleanCSS({ compatibility: "ie8" }))
+      //запускаем стрим
+      .pipe(browserSync.stream())
+  );
 });
+
+gulp.task("watch", function () {
+  gulp.watch(
+    "src/sass/*.+(sass|scss)",
+    gulp.parallel("styles"),
+    browserSync.stream()
+  );
+  gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task("default", gulp.parallel("watch", "styles", "server"));
