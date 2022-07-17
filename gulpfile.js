6905fc9445ba91eecd03501bdcd69a6d81121326
HelHitch <olena9002@gmail.com>
2022-07-17 13:06:30 +0400

    gulp sass and server added

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync");
+const sass = require("gulp-sass").require("sass");
+
+// Static server
+gulp.task("server", function () {
+  browserSync.init({
+    server: {
+      baseDir: "src",
+    },
+  });
+});
+
+gulp.task("styles", function () {
+  return gulp
+    .src("src/sass/*.+(scss|sass)") //даем тип препроцессора на выбор
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
